1eb36f0afef0af89253e4ec889570b6a1a3ebd12
c0a242116f <c0a242116f@edu.teu.ac.jp>
2025-09-30 14:49:46 +0900

    練習3,4

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -5,13 +5,27 @@ import pygame as pg
 
 
 WIDTH, HEIGHT = 1100, 650
-DELTA={
-    pg.K_UP:(0,-5),
-    pg.K_DOWN:(0,+5),
-    pg.K_LEFT:(-5,0),
-    pg.K_RIGHT:(+5,0)
+DELTA = {
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0),
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    引数：こうかとんRect or ばくだんRect
+    戻り値：判定結果タプル（横方向，縦方向）
+    画面内ならTrue/画面外ならFalse
+    """
+    yoko, tate = True, True
+    if rct.left < 0 or WIDTH < rct.right:  # 横方向にはみ出ていたら
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom: # 縦方向にはみ出ていたら
+        tate = False
+    return yoko, tate
 
 
 def main():
@@ -21,13 +35,13 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
-    bb_img=pg.Surface((20,20))
-    pg.draw.circle(bb_img,(255,0,0),(10,10),10)
-    bb_img.set_colorkey((0,0,0))
-    bb_rct=bb_img.get_rect()
-    bb_rct.centerx=random.randint(0,WIDTH)
-    bb_rct.centery=random.randint(0,HEIGHT)
-    vx,vy=+5,+5
+    bb_img = pg.Surface((20, 20))
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+    bb_img.set_colorkey((0, 0, 0))
+    bb_rct = bb_img.get_rect()
+    bb_rct.centerx = random.randint(0, WIDTH)
+    bb_rct.centery = random.randint(0, HEIGHT)
+    vx, vy = +5, +5
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -35,13 +49,17 @@ def main():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
+        if kk_rct.colliderect(bb_rct):
+            return
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for key, mv in DELTA.items():
             if key_lst[key]:
-                sum_mv[0]+=mv[0]
-                sum_mv[1]+=mv[1]
+                sum_mv[0] += mv[0]  # 横方向の移動量を加算
+                sum_mv[1] += mv[1]  # 縦方向の移動量を加算
+        # if key_lst[pg.K_w]:
+        #     sum_mv[1] -= 5
         # if key_lst[pg.K_UP]:
         #     sum_mv[1] -= 5
         # if key_lst[pg.K_DOWN]:
@@ -51,8 +69,15 @@ def main():
         # if key_lst[pg.K_RIGHT]:
         #     sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
-        bb_rct.move_ip(vx,vy)
+        bb_rct.move_ip(vx, vy)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:  # 横方向にはみ出ていたら
+            vx *= -1
+        if not tate:  # 縦方向にはみ出ていたら
+            vy *= -1
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
